bd88da7c098b8d28d27cec01f8327c9b25686731
Junbo Le <78240135+skywalker23241@users.noreply.github.com>
2025-04-15 14:34:57 +0800

    更新页脚和头部社交链接，修改网站标题和描述

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'J.Coopers_Log';
+export const SITE_DESCRIPTION = 'Share what I want to share.';
